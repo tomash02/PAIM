c9c0f3e874a81921286d54ad7696f091e51e728e
tomas <cedzyna02@gmail.com>
2024-12-22 20:33:52 +0100

    additional changes for vercel settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Use relative paths for assets
   build: {
-    outDir: "dist", // Explicitly set the output directory
+    outDir: "dist",
   },
 });
